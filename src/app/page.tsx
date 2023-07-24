05697feda3288b11fbb46f9b0029a5c692405c22
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-24 20:35:32 +0100

    feat: add feature products component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
-import { Heading, Text, Link, Flex } from '@/utils/chakra-components';
+import FeatureProducts from '@/components/feature-products';
+import {
+  Heading,
+  Text,
+  Link,
+  Flex,
+  Container,
+  Box,
+} from '@/utils/chakra-components';
 
 export default function Home() {
   return (
-    <Flex
-      as="main"
-      p="8"
-      justify={'center'}
-      align={'center'}
-      flexDir="column"
-      minH="100vh"
-    >
-      <Heading mt="-12">Work in progress...</Heading>
-      <Text>
-        Check out the style guide{' '}
-        <Link href="/style-guide" color={'aph.primary.100'}>
-          here
-        </Link>
-      </Text>
-    </Flex>
+    <>
+      <Container maxW="container.lg" px="0">
+        <Box mt="120px" mb="168px">
+          <FeatureProducts />
+        </Box>
+      </Container>
+      <Flex
+        as="main"
+        p="8"
+        justify={'center'}
+        align={'center'}
+        flexDir="column"
+        minH="100vh"
+      >
+        <Heading mt="-12">Work in progress...</Heading>
+        <Text>
+          Check out the style guide{' '}
+          <Link href="/style-guide" color={'aph.primary.100'}>
+            here
+          </Link>
+        </Text>
+      </Flex>
+    </>
   );
 }
---
+++ src/components/feature-products.tsx
@@ -0,0 +1,92 @@
+import { Box, Flex, Heading, Image, VStack } from '@/utils/chakra-components';
+import ShopLink from './shop-link';
+
+const FeatureProducts = () => {
+  return (
+    <Flex columnGap="30px" justify="space-between" align="baseline">
+      <VStack align="center" spacing="15px" w="full" h="284px" pos="relative">
+        <Box w="65%" pos="absolute" zIndex="10">
+          <Image
+            src="/assets/shared/desktop/image-category-thumbnail-headphones.png"
+            alt=""
+          />
+        </Box>
+        <VStack
+          pos="absolute"
+          bottom="0px"
+          left="0px"
+          width="100%"
+          h="204px"
+          bg="#F1F1F1"
+          borderRadius="lg"
+          pb="30px"
+          spacing="15px"
+          justify="flex-end"
+        >
+          <Heading as="h6" variant="h6">
+            HEADPHONES
+          </Heading>
+          <ShopLink to="/" w="160px">
+            Shop
+          </ShopLink>
+        </VStack>
+      </VStack>
+      <VStack align="center" spacing="15px" w="full" h="284px" pos="relative">
+        <Box w="65%" pos="absolute" zIndex="10">
+          <Image
+            src="/assets/shared/desktop/image-category-thumbnail-speakers.png"
+            alt=""
+          />
+        </Box>
+        <VStack
+          pos="absolute"
+          bottom="0px"
+          left="0px"
+          width="100%"
+          h="204px"
+          bg="#F1F1F1"
+          borderRadius="lg"
+          pb="30px"
+          spacing="15px"
+          justify="flex-end"
+        >
+          <Heading as="h6" variant="h6">
+            SPEAKERS
+          </Heading>
+          <ShopLink to="/" w="160px">
+            Shop
+          </ShopLink>
+        </VStack>
+      </VStack>
+      <VStack align="center" spacing="15px" w="full" h="274px" pos="relative">
+        <Box w="65%" pos="absolute" zIndex="10">
+          <Image
+            src="/assets/shared/desktop/image-category-thumbnail-earphones.png"
+            alt=""
+          />
+        </Box>
+        <VStack
+          pos="absolute"
+          bottom="0px"
+          left="0px"
+          width="100%"
+          h="204px"
+          bg="#F1F1F1"
+          borderRadius="lg"
+          pb="30px"
+          spacing="15px"
+          justify="flex-end"
+        >
+          <Heading as="h6" variant="h6">
+            EARPHONES
+          </Heading>
+          <ShopLink to="/" w="160px">
+            Shop
+          </ShopLink>
+        </VStack>
+      </VStack>
+    </Flex>
+  );
+};
+
+export default FeatureProducts;
