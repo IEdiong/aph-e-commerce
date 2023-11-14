82d216180959cac5fb8797400cc3f759ded66502
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-11-14 12:27:54 +0100

    resolve redux-persist storage issue
    - Below is a link to the issue and the best solution so far.
    
    - https://github.com/vercel/next.js/discussions/15687

--- src/state/store.ts
+++ src/state/store.ts
@@ -9,7 +9,26 @@ import {
   REHYDRATE,
   persistReducer,
 } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key: string) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: string, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: string) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== 'undefined'
+    ? createWebStorage('local')
+    : createNoopStorage();
 
 const persistConfig = {
   key: 'aphEcommerce',
